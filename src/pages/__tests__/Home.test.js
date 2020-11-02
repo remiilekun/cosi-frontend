00191e83592190f68f975168a7f7ec419ecd35f6
Remilekun Salami <remilekun.salami@gmail.com>
2020-11-03 00:06:22 +0100

    test: home page

---
+++ src/pages/__tests__/Home.test.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import queryString from 'query-string';
+import Home from '../Home';
+import { render, fireEvent, act } from '@testing-library/react';
+import { ThemeWrapper } from 'lib/testHelpers';
+
+const mockFlightNumber = '123456';
+const mockLastName = 'doe';
+
+const mockData = {
+  flightNumber: mockFlightNumber,
+  lastName: mockLastName,
+};
+
+describe('Home page', () => {
+  test('should watch input correctly', () => {
+    const { container } = render(
+      <ThemeWrapper>
+        <Home />
+      </ThemeWrapper>,
+    );
+
+    const flightNumber = container.querySelector("input[name='flightNumber']");
+    const lastName = container.querySelector("input[name='lastName']");
+
+    fireEvent.input(flightNumber, {
+      target: {
+        value: mockFlightNumber,
+      },
+    });
+    fireEvent.input(lastName, {
+      target: {
+        value: mockLastName,
+      },
+    });
+    expect(flightNumber.value).toEqual(mockFlightNumber);
+    expect(lastName.value).toEqual(mockLastName);
+  });
+
+  test('should display correct error message for invalid flight number', async () => {
+    const { container } = render(
+      <ThemeWrapper>
+        <Home />
+      </ThemeWrapper>,
+    );
+    const flightNumber = container.querySelector("input[name='flightNumber']");
+    const lastName = container.querySelector("input[name='lastName']");
+    const submitButton = container.querySelector("button[type='submit']");
+
+    fireEvent.input(flightNumber, {
+      target: {
+        value: `${mockFlightNumber}re`,
+      },
+    });
+    fireEvent.input(lastName, {
+      target: {
+        value: mockLastName,
+      },
+    });
+
+    await act(async () => {
+      fireEvent.submit(submitButton);
+    });
+
+    expect(container.textContent).toMatch(/Flight number should contain only numbers/);
+  });
+
+  test('Should submit form successfully', async () => {
+    const history = {
+      push: jest.fn(),
+    };
+
+    const { container } = render(
+      <ThemeWrapper>
+        <Home history={history} />
+      </ThemeWrapper>,
+    );
+
+    const flightNumber = container.querySelector("input[name='flightNumber']");
+    const lastName = container.querySelector("input[name='lastName']");
+    const submitButton = container.querySelector("button[type='submit']");
+
+    fireEvent.input(flightNumber, {
+      target: {
+        value: mockFlightNumber,
+      },
+    });
+    fireEvent.input(lastName, {
+      target: {
+        value: mockLastName,
+      },
+    });
+
+    await act(async () => {
+      fireEvent.submit(submitButton);
+    });
+    expect(history.push).toHaveBeenCalledWith({
+      pathname: '/check-in',
+      search: `?${queryString.stringify(mockData)}`,
+    });
+  });
+});
